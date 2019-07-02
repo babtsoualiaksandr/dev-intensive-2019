ebd59ed33357e2070a0399d51457c94f2a357e81
babtsoualiaksandr <abob03051972>
2019-07-02 20:27:13 +0300

    hometask_2 complected full rhjvt 360-60 Initial

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -2,7 +2,7 @@ package ru.skillbranch.devintensive.models
 
 import java.util.*
 
-abstract class BaseMessage(
+abstract class BaseMessage_old(
     val id: String,
     val from: User?,
     val chat: Chat,
@@ -21,6 +21,30 @@ abstract class BaseMessage(
                 "image"-> ImageMessage("$lastId", from, chat, date = date, image = payload as String)
                 else-> TextMessage("$lastId", from, chat, date = date, text = payload as String)
 
+            }
+        }
+    }
+}
+
+abstract class BaseMessage(
+    val id: String,
+    val from: User?,
+    val chat: Chat,
+    val isIncoming: Boolean = false,
+    val date: Date = Date()
+) {
+
+    abstract fun formatMessage(): String
+
+    companion object AbstractFactory {
+        var lastId = -1
+
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String, payload: Any?, isIncoming: Boolean = false): BaseMessage {
+            lastId++
+
+            return when (type) {
+                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
+                else -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
             }
         }
     }
