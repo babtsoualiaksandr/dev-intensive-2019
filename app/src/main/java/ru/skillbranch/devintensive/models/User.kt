d21ee655e1d5491cb6f7026d6c18857ea4c65a38
babtsoualiaksandr <abob03051972>
2019-07-02 10:49:23 +0300

    hometask_2 complected full

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -2,93 +2,61 @@ package ru.skillbranch.devintensive.models
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
-data class User(
-    val id : String,
-    var firstName : String?,
-    var lastName : String?,
-    var avatar : String?,
-    var rating : Int = 0,
-    var respect : Int = 0,
-    var lastVisit : Date? = Date(),
+data class User private constructor(val builder: Builder)
+{
+    val id : String
+    var firstName : String?
+    var lastName : String? = String()
+    var avatar : String?
+    var rating : Int = 0
+    var respect : Int = 0
+    var lastVisit : Date? = Date()
     var isOnline : Boolean = false
 
-) {
-    constructor(id: String , firstName: String? , lastName: String?) : this(
-        id = id ,
-        firstName = firstName ,
-        lastName = lastName ,
-        avatar = null
-    )
-
-
-
-    constructor(id: String) : this(id , "John" , "Doe")
-
-
     init {
-
-
-        println(
-            "It's Alive!!! \n" +
-                    "${if (lastName === "Doe") "His name id $firstName  $lastName" else "And his name is $firstName $lastName !!"} \n"
-        )
-
-        getIntro()
+        id = builder.id
+        firstName = builder.firstName
+        lastName = builder.lastName
+        avatar = builder.avatar
+        rating = builder.rating
+        respect = builder.respect
+        lastVisit = builder.lastVisit
+        isOnline = builder.isOnline
+        println("User created. His name is $firstName $lastName")
     }
 
-    private fun getIntro() = """
-            $firstName $lastName
+    companion object Factory{
+        private var lastId : Int = -1
 
-    """.trimIndent()
+        fun makeUser(fullName:String?) : User{
+            lastId ++
 
-    fun printMe() = println(
-        """
-                id: $id
-                firstName: $firstName
-                lastName: $lastName
-                avatar: $avatar
-                rating: $rating
-                respect: $respect
-                lastVisit: $lastVisit
-                isOnline: $isOnline
-        """.trimIndent()
-    )
+            val (firstName, lastName) = Utils.parseFullName(fullName)
 
-
-    companion object Factory {
-        private var lastId: Int = -1
-        fun makeUser(fullName: String): User {
-            lastId++
-            val (firstName , lastName) = Utils.parseFullName(fullName)
-            return User(id = "$lastId" , firstName = firstName , lastName = lastName)
+            return User.Builder().id("$lastId").firstName(firstName).lastName(lastName).build()
         }
     }
 
+    class Builder {
+        var id : String = String()
+        var firstName : String?= String()
+        var lastName : String?= String()
+        var avatar : String?= String()
+        var rating : Int = 0
+        var respect : Int = 0
+        var lastVisit : Date? = Date()
+        var isOnline : Boolean = false
 
-    data class Builder(
-        var id: String ,
-        var firstName: String? ,
-        var lastName: String? ,
-        var avatar: String? ,
-        var rating: Int = 0 ,
-        var respect: Int = 0 ,
-        var lastVisit: Date? ,
-        var isOnline: Boolean
-    ) {
 
         fun id(id: String) = apply { this.id = id }
-        fun firstName(firstName: String) = apply { this.firstName = firstName }
-        fun lastName(lastName: String) = apply { this.lastName = lastName }
-        fun avatar(avatar: String) = apply { this.avatar = avatar }
+        fun firstName(firstName: String?) = apply { this.firstName = firstName }
+        fun lastName(lastName: String?) = apply { this.lastName = lastName }
+        fun avatar(avatar: String?) = apply { this.avatar = avatar }
         fun rating(rating: Int) = apply { this.rating = rating }
         fun respect(respect: Int) = apply { this.respect = respect }
-        fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit }
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
         fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
-
-
-        fun build() = User(id , firstName , lastName , avatar , rating , respect , lastVisit , isOnline)
+        fun build() = User(this)
     }
-
-
 }
 
