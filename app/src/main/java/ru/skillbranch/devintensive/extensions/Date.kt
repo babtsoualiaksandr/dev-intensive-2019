dec7f3a1cb25b16a45a23c5f235c2de5b45a199e
babtsoualiaksandr <abob03051972>
2019-07-02 10:58:47 +0300

    hometask_2 complected full new

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import java.util.*
 import kotlin.math.absoluteValue
+import kotlin.math.abs
 
 const val SECOND = 1000L
 const val MINUTE = 60* SECOND
@@ -66,7 +67,7 @@ fun num2str(n: Int , units: TimeUnits = TimeUnits.SECOND): String {
     return "$nn ${text_forms[2]}"
 }
 
-fun Date.humanizeDiff(date: Date= Date()): String {
+fun Date.humanizeDiff_My(date: Date= Date()): String {
     println(this.time)
     println(date.time)
 
@@ -104,5 +105,75 @@ fun Date.humanizeDiff(date: Date= Date()): String {
     }
 
 
+
+
+}
+
+fun Date.humanizeDiff(date: Date = Date()): String {
+    val diffInMillies = ((this.time - date.time) / SECOND).toInt()
+
+    val minute = 60
+    val hour = 60 * 60
+    val day = 24 * hour
+
+    return when (diffInMillies) {
+        in -45..-1 -> "несколько секунд назад"
+        in -75..-45 -> "минуту назад"
+        in -45 * minute..-75 -> "${minutesAsPlular(abs(diffInMillies / 60))} назад"
+        in -75 * minute..-45 * minute -> "час назад"
+        in -22 * hour..-75 * minute -> "${hoursAsPlular(abs(diffInMillies / 60 / 60))} назад"
+        in -26 * hour..-22 * hour -> "день назад"
+        in -360 * day..-26 * hour -> "${daysAsPlular(abs(diffInMillies / 60 / 60 / 24))} назад"
+        in -Int.MAX_VALUE..-360 * day -> "более года назад"
+
+        in -1..1 -> "только что"
+
+        in 1..45 -> "через несколько секунд"
+        in 45..75 -> "через минуту"
+        in 75..45 * minute -> "через ${minutesAsPlular(diffInMillies / 60)}"
+        in 45 * minute..75 * minute -> "через час"
+        in 75 * minute..22 * hour -> "через ${hoursAsPlular(diffInMillies / 60 / 60)}"
+        in 22 * hour..26 * hour -> "через день"
+        in 26 * hour..360 * day -> "через ${daysAsPlular(diffInMillies / 60 / 60 / 24)}"
+        in 360 * day..Int.MAX_VALUE -> "более чем через год"
+
+        else -> "более года назад"
+    }
+}
+
+
+enum class Plular {
+    ONE,
+    FEW,
+    MANY
+}
+
+private fun minutesAsPlular(value: Int) = when (value.asPlulars()) {
+    Plular.ONE -> "$value минуту"
+    Plular.FEW -> "$value минуты"
+    Plular.MANY -> "$value минут"
+}
+
+private fun hoursAsPlular(value: Int) = when (value.asPlulars()) {
+    Plular.ONE -> "$value час"
+    Plular.FEW -> "$value часа"
+    Plular.MANY -> "$value часов"
+}
+
+private fun daysAsPlular(value: Int) = when (value.asPlulars()) {
+    Plular.ONE -> "$value день"
+    Plular.FEW -> "$value дня"
+    Plular.MANY -> "$value дней"
+}
+
+fun Int.asPlulars() : Plular {
+    if (this > 10 && this % 100 / 10 == 1) return Plular.MANY
+    if (this == 0 || this == 1) return Plular.ONE
+
+    return when (this % 10) {
+        1 -> Plular.ONE
+        2, 3, 4 -> Plular.FEW
+        else -> Plular.MANY
+    }
 }
 
