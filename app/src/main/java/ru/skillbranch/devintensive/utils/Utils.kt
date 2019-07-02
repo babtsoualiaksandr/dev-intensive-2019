afe51f8300225e813536eff07c5d25e1ba832e77
babtsoualiaksandr <abob03051972>
2019-07-02 11:27:35 +0300

    hometask_2 complected full new2

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -32,96 +32,132 @@ object Utils {
 
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        if (firstName.isNullOrBlank() && lastName.isNullOrBlank())
+
+        if ((firstName==null)||(firstName==" ")|| (firstName=="") && (lastName==null) || (firstName==" ")|| (firstName=="")) {
             return null
+        }
 
-        val fNameInitial = firstName?.first()
-        val lNameInitial = lastName?.first()
+        var i1: String?
+        var i2: String?
 
-        return (when {
-            fNameInitial == null -> "$lNameInitial"
-            lNameInitial == null -> "$fNameInitial"
-            else -> "$fNameInitial$lNameInitial"
-        }).toUpperCase()
-    }
+        when (firstName) {
+            null, "", " " -> i1= null
+            else -> i1 = firstName[0].toString().toUpperCase()
+        }
 
-    fun transliteration(payload: String, divider: String = " "): String {
-        var result = String()
+        when (lastName) {
+            null, "", " "  -> i2= null
+            else -> i2 = lastName[0].toString().toUpperCase()
+        }
 
-        for (symbol in payload){
-            if(symbol == ' ') result += divider
-            if(translitMap.containsKey(symbol))
-                result += translitMap[symbol]
+        var result: String?
+        if ((i1 == null) && (i2 != null))
+        {
+            result = i2
+        } else {
+            if ((i1 != null) && (i2 == null)) {
+                result = i1
+            } else {
+                result = "$i1$i2"
+            }
         }
 
         return result
     }
 
-    private val translitMap = mutableMapOf(
-        'а' to "a",
-        'б' to "b",
-        'в' to "v",
-        'г' to "g",
-        'д' to "d",
-        'е' to "e",
-        'ё' to "e",
-        'ж' to "zh",
-        'з' to "z",
-        'и' to "i",
-        'й' to "i",
-        'к' to "k",
-        'л' to "l",
-        'м' to "m",
-        'н' to "n",
-        'о' to "o",
-        'п' to "p",
-        'р' to "r",
-        'с' to "s",
-        'т' to "t",
-        'у' to "u",
-        'ф' to "f",
-        'х' to "h",
-        'ц' to "c",
-        'ч' to "ch",
-        'ш' to "sh",
-        'щ' to "sh",
-        'ъ' to "",
-        'ь' to "",
-        'э' to "e",
-        'ю' to "yu",
-        'я' to "ya",
+    fun transliteration(payload: String, divider: String? = " "): String {
 
-        'А' to "A",
-        'Б' to "B",
-        'В' to "V",
-        'Г' to "G",
-        'Д' to "D",
-        'Е' to "E",
-        'Ё' to "E",
-        'Ж' to "Zh",
-        'З' to "Z",
-        'И' to "I",
-        'Й' to "I",
-        'К' to "K",
-        'Л' to "L",
-        'М' to "M",
-        'Н' to "N",
-        'О' to "O",
-        'П' to "P",
-        'Р' to "R",
-        'С' to "S",
-        'Т' to "T",
-        'У' to "U",
-        'Ф' to "F",
-        'Х' to "H",
-        'Ц' to "C",
-        'Ч' to "Ch",
-        'Ш' to "Sh",
-        'Щ' to "Sh",
-        'Ъ' to "",
-        'Ь' to "",
-        'Э' to "E",
-        'Ю' to "Yu",
-        'Я' to "Ya"
-    )
+        val lexicon = mapOf(
+            "а" to "a",
+            "б" to "b",
+            "в" to "v",
+            "г" to "g",
+            "д" to "d",
+            "е" to "e",
+            "ё" to "e",
+            "ж" to "zh",
+            "з" to "z",
+            "и" to "i",
+            "й" to "i",
+            "к" to "k",
+            "л" to "l",
+            "м" to "m",
+            "н" to "n",
+            "о" to "o",
+            "п" to "p",
+            "р" to "r",
+            "с" to "s",
+            "т" to "t",
+            "у" to "u",
+            "ф" to "f",
+            "х" to "h",
+            "ц" to "c",
+            "ч" to "ch",
+            "ш" to "sh",
+            "щ" to "sh'",
+            "ъ" to "",
+            "ы" to "i",
+            "ь" to "",
+            "э" to "e",
+            "ю" to "yu",
+            "я" to "ya",
+
+            "А" to "A",
+            "Б" to "B",
+            "В" to "V",
+            "Г" to "G",
+            "Д" to "D",
+            "Е" to "E",
+            "Ё" to "E",
+            "Ж" to "ZH",
+            "З" to "Z",
+            "И" to "I",
+            "Й" to "I",
+            "К" to "K",
+            "Л" to "L",
+            "М" to "M",
+            "Н" to "N",
+            "О" to "O",
+            "П" to "P",
+            "Р" to "R",
+            "С" to "S",
+            "Т" to "T",
+            "У" to "U",
+            "Ф" to "F",
+            "Х" to "H",
+            "Ц" to "C",
+            "Ч" to "CH",
+            "Ш" to "SH",
+            "Щ" to "SH'",
+            "Ъ" to "",
+            "Ы" to "I",
+            "Ь" to "",
+            "Э" to "E",
+            "Ю" to "YU",
+            "Я" to "YA"
+        )
+
+        var parts: List<String> =  payload?.split(divider.orEmpty())
+        var resultList: MutableList<String> = mutableListOf()
+
+        for (partsitem in parts) {
+            val builder = StringBuilder()
+            for (i in 0..partsitem.length-1) {
+                if (partsitem[i].toString() in lexicon) {
+                    val item = if (i==0) {lexicon[partsitem[i].toString().toUpperCase()]} else { lexicon[partsitem[i].toString()]}
+                    builder.append(item)
+                } else if (i==0) { builder.append(partsitem[i].toString().toUpperCase())} else { builder.append(partsitem[i].toString()) }
+            }
+            resultList.add(builder.toString())
+        }
+
+        var result: String=""
+        for (i in 0..resultList.count()-1) {
+            if (i!= resultList.count()-1) {result = result + resultList[i] + divider} else {result = result + resultList[i]}
+        }
+        return result
+    }
+
+
+
 }
